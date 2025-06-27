cf479cc00a566fba8977331c4f0958bb742da6c0
sawyer bristol <sawyerbristol@gmail.com>
2025-06-27 12:23:05 -0600

    diy framebuffer

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ use embassy_time::Timer;
 use embedded_hal_bus::spi::ExclusiveDevice;
 use embedded_sdmmc::asynchronous::{File, SdCard, ShortFileName, VolumeIdx, VolumeManager};
 use static_cell::StaticCell;
+use talc::*;
 
 mod peripherals;
 use peripherals::{keyboard::KeyEvent, peripherals_task};
@@ -32,6 +33,16 @@ embassy_rp::bind_interrupts!(struct Irqs {
     I2C1_IRQ => i2c::InterruptHandler<I2C1>;
 });
 
+static mut ARENA: [u8; 300_000] = [0; 300_000];
+
+#[global_allocator]
+static ALLOCATOR: Talck<spin::Mutex<()>, ClaimOnOom> = Talc::new(unsafe {
+    // if we're in a hosted environment, the Rust runtime may allocate before
+    // main() is called, so we need to initialize the arena automatically
+    ClaimOnOom::new(Span::from_array(core::ptr::addr_of!(ARENA).cast_mut()))
+})
+.lock();
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
@@ -39,19 +50,21 @@ async fn main(spawner: Spawner) {
     static KEYBOARD_EVENTS: StaticCell<Channel<NoopRawMutex, KeyEvent, 10>> = StaticCell::new();
     let keyboard_events = KEYBOARD_EVENTS.init(Channel::new());
 
-    // configure keyboard event handler
-    let mut config = i2c::Config::default();
-    config.frequency = 100_000;
-    let i2c1 = I2c::new_async(p.I2C1, p.PIN_7, p.PIN_6, Irqs, config);
-    spawner
-        .spawn(peripherals_task(i2c1, keyboard_events.sender()))
-        .unwrap();
-
-    // // configure display handler
-    // let mut config = spi::Config::default();
-    // config.frequency = 16_000_000;
-    // let spi1 = spi::Spi::new_blocking(p.SPI1, p.PIN_10, p.PIN_11, p.PIN_12, config);
+    // // configure keyboard event handler
+    // let mut config = i2c::Config::default();
+    // config.frequency = 100_000;
+    // let i2c1 = I2c::new_async(p.I2C1, p.PIN_7, p.PIN_6, Irqs, config);
     // spawner
-    //     .spawn(display_task(spi1, p.PIN_13, p.PIN_14, p.PIN_15))
+    //     .spawn(peripherals_task(i2c1, keyboard_events.sender()))
     //     .unwrap();
+
+    // configure display handler
+    let mut config = spi::Config::default();
+    config.frequency = 16_000_000;
+    let spi1 = spi::Spi::new(
+        p.SPI1, p.PIN_10, p.PIN_11, p.PIN_12, p.DMA_CH0, p.DMA_CH1, config,
+    );
+    spawner
+        .spawn(display_task(spi1, p.PIN_13, p.PIN_14, p.PIN_15))
+        .unwrap();
 }
