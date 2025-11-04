ee0d2880bb350dabca594f9b846522550ecd8e3a
sawyer bristol <sawyerbristol@gmail.com>
2025-11-04 13:43:48 -0700

    selection ui for user apps
    works

--- user-apps/gif/src/main.rs
+++ user-apps/gif/src/main.rs
@@ -4,16 +4,17 @@
 extern crate alloc;
 use abi::{
     display::Display,
-    fs::{file_len, read_file},
+    fs::{Entries, file_len, list_dir, read_file},
     get_key, get_ms,
     keyboard::{KeyCode, KeyState},
     print, sleep,
 };
-use alloc::vec;
+use alloc::{format, vec, vec::Vec};
 use core::panic::PanicInfo;
 use embedded_graphics::{
     image::ImageDrawable, pixelcolor::Rgb565, prelude::Point, transform::Transform,
 };
+use selection_ui::SelectionUi;
 use tinygif::Gif;
 
 #[panic_handler]
@@ -31,13 +32,27 @@ pub fn main() {
     print!("Starting Gif app");
     let mut display = Display;
 
-    let size = file_len("/gifs/bad_apple.gif");
+    let mut entries = Entries::new();
+    list_dir("/gifs", &mut entries);
+
+    let mut files = entries.entries();
+    files.retain(|e| e.extension().unwrap_or("") == "gif");
+    let gifs = &files.iter().map(|e| e.full_name()).collect::<Vec<&str>>();
+
+    let mut selection_ui = SelectionUi::new(&gifs, "No Gif files found in /gifs");
+    let selection = selection_ui
+        .run_selection_ui(&mut display)
+        .expect("failed to draw")
+        .expect("Failed to get user selection");
+
+    let file_name = format!("/gifs/{}", gifs[selection]);
+    let size = file_len(&file_name);
     let mut buf = vec![0_u8; size];
-    let read = read_file("/gifs/bad_apple.gif", 0, &mut buf);
+    let read = read_file(&file_name, 0, &mut buf);
     print!("read: {}, file size: {}", read, size);
     assert!(read == size);
 
-    let gif = Gif::<Rgb565>::from_slice(&buf).unwrap();
+    let gif = Gif::<Rgb565>::from_slice(&buf).expect("Failed to parse gif");
     let height = gif.height();
 
     let mut frame_num = 0;
